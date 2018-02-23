b06a4790b4c6d0eea097974413619c8c823cfe04
Ubuntu <ubuntu@ip-172-31-93-211.ec2.internal>
2018-02-23 23:44:58 +0000

    comment change added

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -25,6 +25,7 @@ public class GameController {
     private Random randomGenerator = new Random();
 
 	// Clicking the "New Game" button on the main page loads the cell seletion page
+	//This is change AP
     @RequestMapping("/new")
     public ModelAndView newGame() {
         ModelAndView mav = new ModelAndView("game/edit");
